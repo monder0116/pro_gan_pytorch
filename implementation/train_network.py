3790fd7fe7ce16218e33d5e3fa937af03c643f37
MEHMET ONDER <m.onder@gtu.edu.tr>
2020-02-29 17:38:52 +0300

    Update train_network.py

--- implementation/train_network.py
+++ implementation/train_network.py
@@ -23,7 +23,7 @@ def parse_arguments():
     :return: args => parsed command line arguments
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", action="store", type=str, default="configs/1.conf",
+    parser.add_argument("--config", action="store", type=str, default="config/1.conf",
                         help="default configuration for the Network")
     parser.add_argument("--start_depth", action="store", type=int, default=0,
                         help="Starting depth for training the network")
